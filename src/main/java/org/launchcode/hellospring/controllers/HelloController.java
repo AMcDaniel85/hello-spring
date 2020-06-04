6f49af7482d1791e011900670a9fa9fc810c7a88
amfzn503 <amfzn503@msn.com>
2020-06-04 12:15:56 -0500

    Thymeleaf practice

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,83 +1,46 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@ResponseBody
-@RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+
 public class HelloController {
 
-    //Handles request at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
-
-    //lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
-
-    //lives at /hello/hello
     //Handles request of the form /hello?name=LaunchCode
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     //handles requests of the form /hello/LaunchCode
-    @GetMapping("/{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "Hello";
     }
 
-//    @GetMapping("form")
-//    public String helloForm(){
-//        String html =
-//                "<html>" +
-//                        "<body>" +
-//                        "<form action='hello' method='post'>" + //submits a request to hello
-//                        "<input type='text' name='name'/>" +
-//                        "<input type='submit' value='Greet me!'/>" +
-//                        "</form>" +
-//                        "</body>" +
-//                        "</html>";
-//        return html;
-//    }
-    @PostMapping("language")
-    public static String displayMessage(ServletRequest request) {
-        String lang = request.getParameter("Language");
-        String name = request.getParameter("name");
-
-        return lang + " " + name + "!";
-    }
-    //exercise 10.5
     @GetMapping("form")
-    public static String createMessage(){
-        String html =
-                "<html>" +
-                        "<body>" +
-                        "<form method = 'post' action='language'>" +
-                        "<input type='text' name='name'/>" +
-                        "<label for='Language'>Select a language:</label>" +
-                        "<select name='Language' id='Language'>" +
-                        "<option value=''>--Please choose a language--</option>" +
-                        "<option value='HELLO'>English</option>" +
-                        "<option value='HOLA'>Spanish</option>" +
-                        "<option value='BONJOUR'>French</option>" +
-                        "<option value='HALLO'>German</option>" +
-                        "<option value='NAMASTE'>Hindi</option>" +
-                        "</select>" +
-                        "<input type='submit' value='Greet me!'/>" +
-                        "</form>" +
-                        "</body>" +
-                        "</html>";
-        return html;
+    public String helloForm(){
+        return "form";
     }
 
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Bob");
+        names.add("Allison");
+        names.add("Fiona");
+        names.add("Anna");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
 
 }
